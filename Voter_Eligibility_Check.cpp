a7e12be6381f004d796f194f6718e0d35ea7b126
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-05 14:52:57 +0600

    Update Voter_Eligibility_Check.cpp

--- Voter_Eligibility_Check.cpp
+++ Voter_Eligibility_Check.cpp
@@ -1,3 +1,51 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+    int age;
+    std::string input;
+
+    while (true) {
+        std::cout << "Enter your age: ";
+        std::getline(std::cin, input);
+        std::stringstream ss(input);
+
+        if (ss >> age && ss.eof()) {
+            break;
+        }
+
+        std::cout << "Invalid age entered. Please enter a valid integer age." << std::endl;
+    }
+
+    if (18 <= age && age <= 120) {
+        std::cout << "Congratulations!" << std::endl;
+        std::cout << "You are eligible to vote." << std::endl;
+    } else if (12 <= age && age < 18) {
+        std::cout << "You are not yet eligible to vote." << std::endl;
+        std::cout << "Enjoy your teenage years!" << std::endl;
+    } else if (0 <= age && age < 12) {
+        std::cout << "You are too young to vote." << std::endl;
+        std::cout << "Make the most of your childhood!" << std::endl;
+    } else if (age < 0) {
+        std::cout << "Invalid age entered." << std::endl;
+        std::cout << "Please enter a positive value." << std::endl;
+    } else {
+        std::cout << "You have surpassed the maximum voting age." << std::endl;
+        std::cout << "Thank you for your contribution to society!" << std::endl;
+    }
+
+    return 0;
+}
+
+
+// -------------------------------END---------------------------------
+
+// The program above is very Dynamic while the program below is Static
+
+// ------------------------------START--------------------------------
+
+
 #include<stdio.h>
 
 int main()

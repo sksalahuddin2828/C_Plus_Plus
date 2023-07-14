08cfce6e4bfc39b97c1ba0b2a2af108cfe97919d
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:19:41 +0600

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------C++ Coding Challenges---------------------------------------------------------
+
+// C++ Coding Challenges on Numbers 
+// Write a program in C++ to -
+
+// 1. Convert decimal numbers to octal numbers.
+// 2. Reverse an integer.
+// 3. Print the Fibonacci series using the recursive method.
+// 4. Return the Nth value from the Fibonacci sequence.
+// 5. Find the average of numbers (with explanations).
+// 6. Convert Celsius to Fahrenheit.
+
+//----------------------------------------------------------------Solution of Problem:----------------------------------------------------------
+
+// 1. Converting Decimal Numbers to Octal Numbers:
+
+//----------------------------------------------------------------------------------------------------------------------------------------------

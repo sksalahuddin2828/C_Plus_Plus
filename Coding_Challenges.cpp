15a5ebe3e7ea9632edd786c13b976b5370ad17bc
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:20:33 +0600

    Update and rename .cpp to Coding_Challenges.cpp

--- Coding_Challenges.cpp
+++ Coding_Challenges.cpp
@@ -14,4 +14,30 @@
 
 // 1. Converting Decimal Numbers to Octal Numbers:
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int decimal_number = 25;
+    int octal_number[100];
+    int i = 0;
+
+    while (decimal_number > 0) {
+        octal_number[i] = decimal_number % 8;
+        decimal_number = decimal_number / 8;
+        i++;
+    }
+
+    cout << "Octal number: ";
+
+    for (int j = i - 1; j >= 0; j--) {
+        cout << octal_number[j];
+    }
+
+    return 0;
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Reversing an Integer:
+

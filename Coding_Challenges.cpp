1ae912d11df6c7c4c121ea605c214c9348bc67e5
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:22:21 +0600

    Update Coding_Challenges.cpp

--- Coding_Challenges.cpp
+++ Coding_Challenges.cpp
@@ -87,6 +87,24 @@ int main() {
 
 // 4. Returning the Nth Value from the Fibonacci Sequence:
 
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n = 10;
+    int fibonacci_number = fibonacci(n);
+    cout << fibonacci_number;
+    return 0;
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
 // 5. 

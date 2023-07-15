e34d43793a844bc15214d185c10f78c51af25b44
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:08:43 +0600

    Update kaprekar_constant.cpp

--- kaprekar_constant.cpp
+++ kaprekar_constant.cpp
@@ -24,3 +24,31 @@ int main() {
     std::cout << "Number of steps to reach Kaprekar constant: " << steps << std::endl;
     return 0;
 }
+
+//-----------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int kaprekar_constant(int n) {
+    int count = 0;
+    while (n != 6174) {
+        count++;
+        std::string digits = std::to_string(n);
+        digits.insert(0, 4 - digits.length(), '0');
+        std::sort(digits.begin(), digits.end());
+        int ascending = std::stoi(digits);
+        std::reverse(digits.begin(), digits.end());
+        int descending = std::stoi(digits);
+        n = descending - ascending;
+    }
+    return count;
+}
+
+int main() {
+    std::cout << kaprekar_constant(1234) << std::endl;  
+    return 0;
+}
+
+// Output: 3

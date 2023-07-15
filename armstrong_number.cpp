3ab6f7cedb6be55ca7453b573e81abb144f45269
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:03:45 +0600

    Update armstrong_number.cpp

--- armstrong_number.cpp
+++ armstrong_number.cpp
@@ -29,3 +29,32 @@ int main() {
     }
     return 0;
 }
+
+//---------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <cmath>
+
+bool is_armstrong_number(int n) {
+    int num_digits = 0;
+    int temp = n;
+    while (temp > 0) {
+        temp /= 10;
+        num_digits++;
+    }
+    int sum_of_powers = 0;
+    temp = n;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum_of_powers += pow(digit, num_digits);
+        temp /= 10;
+    }
+    return n == sum_of_powers;
+}
+
+int main() {
+    std::cout << (is_armstrong_number(153) ? "True" : "False") << std::endl;  
+    return 0;
+}
+
+// Output: True
